d9491d023afd0acfd7ee663d447f1f99708ca0d0
StefanS <stefan.github.notifications@gmail.com>
2022-11-28 17:41:43 -0600

    Add files via upload

---
+++ library_.r
@@ -0,0 +1,47 @@
+
+#library(plyr)
+
+#library(MASS)
+
+library(tidyverse) # Activates ggplot2, dplyr, tidyr, readr, purr, tibble, stringr, forcats
+
+library(mice) # Machine learning for missing data imputation using e.g. "rf"
+ 
+library(forecast) # includes ARIMA, fourier, etc.
+ 
+#library(keras)
+
+#library(tensorflow)
+ 
+#library(reticulate)
+
+library(lubridate)
+
+library(readr)
+
+library(scales)
+
+# library(MVN)
+# 
+# library(sgeostat)
+
+options(max.print = 35, scipen=999)
+
+#library("fUnitRoots")
+
+library(lmtest)
+
+#library(urca)
+
+library(tseries)
+
+#library(seastests)
+
+#library(scatterplot3d)
+
+library(cowplot)
+
+library(plotly)
+
+
+
